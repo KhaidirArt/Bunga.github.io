30778778ac5706cc1a4a2303dbcf6e00d01bd61e
KhaidirArt <42932461+KhaidirArt@users.noreply.github.com>
2020-10-23 06:02:36 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -197,7 +197,7 @@ class App extends Component {
               className={this.state.bgcolor !== "#000000" ? "hidden" : "show"}
             >
               <h1 className="baffle">
-                I LOVE YOU GITA SAYANG 
+                I LOVE YOU GITA SAYANG ❤
               </h1>
             </div>
           </div>
