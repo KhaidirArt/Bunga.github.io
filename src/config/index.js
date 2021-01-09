466443bdbc0916c9596274758239537c99fb5abc
KhaidirArt <42932461+KhaidirArt@users.noreply.github.com>
2021-01-10 01:17:51 +0700

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -7,7 +7,6 @@ export default {
     "Bunga bersemi di langit-langit, say you love me like u mean it😉",
     "Kembang mengkudu budidaya argonomi, what can i du to make u love me❤️",
     "Canting dan Kayu dibawa Bayu, Nothing's gonna change my love for You💖",
-    "Malam-malam kerumah kawan disuruh ambil kayu, I'am the one Who want's to be with you🌹",
   ],
   panggilan: ["Giyang", "Gita Sayang", "Ayaaaaank Gita", "Gita", "Gitakuh Sayang",],
   colors: [
